440a337b0c27f699e473e17f1d6f5cb7699de63c
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-19 15:00:24 +0700

    implement templating dengan mustache.java

--- src/test/java/id/ac/tazkia/notifikasi/gmail/NotifikasiGmailApplicationTests.java
+++ src/test/java/id/ac/tazkia/notifikasi/gmail/NotifikasiGmailApplicationTests.java
@@ -1,5 +1,8 @@
 package id.ac.tazkia.notifikasi.gmail;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import id.ac.tazkia.notifikasi.gmail.service.GmailApiService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +13,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StreamUtils;
 
+import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,10 +30,17 @@ public class NotifikasiGmailApplicationTests {
 
 	@Test
 	public void testSendGmail() throws Exception {
-        String emailContent = StreamUtils.copyToString(mailTemplate.getInputStream(),
+        String emailTemplate = StreamUtils.copyToString(mailTemplate.getInputStream(),
                 Charset.forName("UTF-8"));
 
-        gmailApiService.send("endy@tazkia.ac.id", "Selamat Datang di Aplikasi Notifikasi", emailContent);
+        Map<String, String> data = new HashMap<>();
+        data.put("customerName", "Endy Muhardin");
+
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache m = mf.compile("templates/welcome.html");
+        StringWriter emailContent = new StringWriter();
+        m.execute(emailContent, data);
+        gmailApiService.send("endy@tazkia.ac.id", "Selamat Datang di Aplikasi Notifikasi", emailContent.toString());
     }
 
 }
