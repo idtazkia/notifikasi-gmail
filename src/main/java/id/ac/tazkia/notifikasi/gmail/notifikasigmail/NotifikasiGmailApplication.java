ee31959886ff9a3caf54519ce5f80c752dd69f9d
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-19 12:49:12 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/notifikasi/gmail/notifikasigmail/NotifikasiGmailApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.notifikasi.gmail.notifikasigmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifikasiGmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifikasiGmailApplication.class, args);
+	}
+}
